4d9220542df0e43b1371d44d26fa5f548e2705b9
Hamidreza Hashemi <101236392+hamidrezahashemi71@users.noreply.github.com>
2026-06-22 08:59:39 +0330

    chore: font variables

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -3,28 +3,48 @@ import localFont from "next/font/local";
 export const iranSans = localFont({
   src: [
     {
-      path: "../app/assets/fonts/IRANSans-UltraLight-web.woff2",
+      path: "../app/assets/fonts/IranSans-thin-100.woff",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../app/assets/fonts/IranSans-ultraLight-200.woff",
       weight: "200",
       style: "normal",
     },
     {
-      path: "../app/assets/fonts/IRANSans-Light-web.woff2",
+      path: "../app/assets/fonts/IranSans-light-300.woff",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../app/assets/fonts/IRANSans-web.woff2",
+      path: "../app/assets/fonts/IranSans-medium-400.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../app/assets/fonts/IRANSans-Medium-web.woff2",
+      path: "../app/assets/fonts/IranSans-regular-500.woff",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../app/assets/fonts/IRANSans-Bold-web.woff2",
+      path: "../app/assets/fonts/IranSans-semibold-600.woff",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../app/assets/fonts/IranSans-bold-700.woff",
       weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../app/assets/fonts/IranSans-extrabold-800.woff",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../app/assets/fonts/IranSans-black-900.woff",
+      weight: "900",
       style: "normal",
     },
   ],
